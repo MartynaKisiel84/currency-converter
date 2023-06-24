c81a25e34aceddce76db97fb2c7fab50bbd90a24
MartynaKisiel84 <128509890+MartynaKisiel84@users.noreply.github.com>
2023-06-24 23:42:30 +0200

    rename folder css

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 {
-const calculateResult = (amount, currency) => {
-    const rateUSD = 4.3915;
-    const rateGBP = 5.2788;
-    const rateEUR = 4.6809;
+    const calculateResult = (amount, currency) => {
+        const rateUSD = 4.3915;
+        const rateGBP = 5.2788;
+        const rateEUR = 4.6809;
 
-    switch (currency) {
-        case "USD":
-        return amount / rateUSD;
-        
-        case "GBP":
-        return amount / rateGBP;
-        
-        case "EUR":
-        return amount / rateEUR;
-    }
-};
+        switch (currency) {
+            case "USD":
+                return amount / rateUSD;
+
+            case "GBP":
+                return amount / rateGBP;
+
+            case "EUR":
+                return amount / rateEUR;
+        }
+    };
 
 
-    const updateResultText =(amount, result, currency) => {
+    const updateResultText = (amount, result, currency) => {
         resultElement.innerHTML = '${amount.toFixed(2)} PLN = <strong>$result.toFixed(2)} ${currency}</strong>';
         const resultElement = document.querySelector(".js-result");
     }
-     
-     const onFormSubmit = (event) => { 
+
+    const onFormSubmit = (event) => {
         event.preventDefault();
-            
+
         const amountElement = document.querySelector(".js-amount");
         const currencyElement = document.querySelector(".js-currency");
-        
-        
+
+
         const amount = +amountElement.value;
         const currency = currencyElement.value;
-    
-    
+
+
         const result = calculateResult(amount, currency);
 
-        updateResultText (amount, result, currency);
-
-    
-};
+        updateResultText(amount, result, currency);
 
 
-    const init = () =>{
+    };
+
+
+    const init = () => {
         const formElement = document.querySelector(".js-form)");
 
-        formElement.addEventListener ("submit", onFormSubmit);
-};
+        formElement.addEventListener("submit", onFormSubmit);
+    };
 
-    init ();
+    init();
 }
